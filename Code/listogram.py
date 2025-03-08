d7d79026d2a69c1b684747fa208b103923c0ab99
JuanzelS <zel.staton@gmail.com>
2025-03-08 14:35:06 -0500

    add new file

--- Code/listogram.py
+++ Code/listogram.py
@@ -20,25 +20,50 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        # If the word already exists, increment its frequency count
+        for entry in self:
+            if entry[0] == word:
+                entry[1] += count
+                break
+        else:
+            # If the word doesn't exist, add a new entry with the count
+            self.append([word, count])
+        # Update the total token count
+        self.tokens += count
+        # Update the distinct word count (types)
+        self.types = len(self)
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        for entry in self:
+            if entry[0] == word:
+                return entry[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
-        # TODO: Check if word is in this histogram
+        for entry in self:
+            if entry[0] == word:
+                return True
+        return False
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
-        # TODO: Implement linear search to find index of entry with target word
+        for index, entry in enumerate(self):
+            if entry[0] == target:
+                return index
+        return None
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
-        # TODO: Randomly choose a word based on its frequency in this histogram
+        # Create a list of words, each repeated by its frequency count
+        weighted_words = []
+        for word, count in self:
+            weighted_words.extend([word] * count)
+        # Randomly sample from this weighted list
+        return random.choice(weighted_words)
 
 
 def print_histogram(word_list):
